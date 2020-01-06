c1c1638aaa6d90b0f83bf1c9a45a2ccd8f6825db
Codi Huston (Home) <codi.huston540@gmail.com>
2020-01-05 23:15:57 -0600

    client: eslint + prettier

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+const x = 0;
 
 function App() {
   return (
