154e9f68ce4a07e9634e85bf796d30666150171c
Huston <cdr95985@ad.wku.edu>
2020-01-06 09:16:20 -0600

    build(server): migrate express to use babel
    - IMPORTANT: rename src/bin/www -> ssrc/bin/www.js so babel actually
    compiles the this file

--- server/src/app.js
+++ server/src/app.js
@@ -1,11 +1,11 @@
-var createError = require("http-errors");
-var express = require("express");
-var path = require("path");
-var cookieParser = require("cookie-parser");
-var logger = require("morgan");
+import { createError } from "http-errors";
+import express from "express";
+import path from "path";
+import cookieParser from "cookie-parser";
+import logger from "morgan";
 
-var indexRouter = require("./routes/index");
-var usersRouter = require("./routes/users");
+import { router as indexRouter } from "./routes/index";
+import { router as usersRouter } from "./routes/users";
 
 var app = express();
 
@@ -38,4 +38,4 @@ app.use(function(err, req, res, next) {
   res.render("error");
 });
 
-module.exports = app;
+export default app;
