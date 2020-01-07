33587e970a5fe456bccf44c91faf9985695eb578
Huston <cdr95985@ad.wku.edu>
2020-01-07 10:44:04 -0600

    feat(server): implements google access token into rest api invocation
    - In a previous commit, I obtained an access token to google via
    passport--which, under the hood--fetch said token similarly to how I do
    in the example/googleapis-nodejs-client branch
    - I then used the token in the authorization header to send an http request to the google REST api
    OAuth2 endpoints
    
    I am probably not going to use the REST methods in this application
    since I think it may be a little "cleaner" to use the nodejs client library. Surely the http endpoints wouldn't change, but if any sort of implementation did, I'd expect their client library to be updated quickly, which would make this app a little more future proof. I also think the code will look less repetitive in terms of the number of http requests we'll end up using to their apis.

--- server/src/routes/auth.js
+++ server/src/routes/auth.js
@@ -1,6 +1,6 @@
 import express from "express";
 import passport from "passport";
-
+import axios from "axios";
 const router = express.Router();
 
 // GET /auth/google
@@ -32,10 +32,29 @@ router.get(
   "/google/cb",
   // TODO: handle failure
   passport.authenticate("google", { failureRedirect: "/login" }),
-  function(req, res) {
+  async function(req, res) {
     // TODO: on successful login, return the JWT to the client side!
     console.log("TODO: return token to client", req.user.token);
-    res.redirect("/");
+
+    /**
+     * You can use the token when sending an HTTP request to the WebAPI as
+     * described in either of the following documentation:
+     * - https://developers.google.com/oauthplayground/?code=4/vAHXVpAMob_uSs3PFJY0AA28VkLgxhg2u34IBf9ypQfmB3xXwUiPhEaf8NSMHOCWWHm-IeFX-WFhLtwFED2Vbz8&scope=https://www.googleapis.com/auth/calendar%20https://www.googleapis.com/auth/calendar.events
+     * - https://developers.google.com/calendar/v3/reference
+     */
+    try {
+      const headers = { Authorization: `Bearer ${req.user.token}` };
+      const response = await axios.get(
+        "https://www.googleapis.com/calendar/v3/users/me/calendarList",
+        {
+          headers
+        }
+      );
+      console.log("The response", response.data);
+      res.json(response.data);
+    } catch (e) {
+      res.json(e);
+    }
   }
 );
 
