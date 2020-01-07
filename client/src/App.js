3382575837c5fd55ee8c9ccc5b86c615d72afe07
Huston <cdr95985@ad.wku.edu>
2020-01-07 12:27:52 -0600

    feat(client): add globals support for ref to backend api
    - refactor GoogleSigninButton with this variable

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
+import "./config/globals";
 import GoogleSignInButton from "./components/GoogleSignInButton";
 
 function App() {
