22c0bb188a19558cc0ebadb50a94f0009f08c819
Huston <cdr95985@ad.wku.edu>
2020-01-07 13:32:54 -0600

    feat(server): implement express-session
    - uses uuid/v4 to ID generate unique session ids
    - session cookies are named gairos.NODE_ENV; configurable in .env
    - upon successfully receiving google access token,
    req.session.isAuthenticated to true, and sets req.session.tokens equal
    to the received tokens...
    
    refs #2 #3

--- server/src/routes/index.js
+++ server/src/routes/index.js
@@ -3,11 +3,12 @@ const router = express.Router();
 
 /* GET home page. */
 router.get("/", function(req, res, next) {
-  console.log(req.user);
+  console.log("session", req.session);
   res.render("index", { title: "Express" });
 });
 
 router.get("/login", function(req, res, next) {
+  console.log("session", req.session);
   res.render("index", { title: "Please log back in" });
 });
 
