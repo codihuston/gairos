6c9995ef93afc0d1234e2ebf40e93be1014ed4a0
Huston <cdr95985@ad.wku.edu>
2020-01-09 17:27:23 -0600

    fix(server): re-add resolvers to user file that went missing in last commit

--- server/src/api/gairos/users/resolvers.js
+++ server/src/api/gairos/users/resolvers.js
@@ -1,7 +1,36 @@
 import { createError } from "apollo-errors";
+import { calendar } from "../../google";
 
 export default {
   Query: {
+    // get access to context
+    hello: (parent, args, context, info) => {
+      const { session } = context;
+
+      console.log("Session in resolvers:", context.session);
+
+      // Handle unauthorized requests
+      // TODO: move into its own /errors file?
+      const isAuthenticated = session.isAuthenticated || false;
+      const hasAccessToken =
+        (session.tokens && session.tokens.access_token) || false;
+
+      const UnauthenticatedError = createError("UnauthenticatedError", {
+        message: "You must log in to do that."
+      });
+
+      if (!isAuthenticated || !hasAccessToken) {
+        throw new UnauthenticatedError();
+      }
+
+      return "Hello world!";
+    },
+    getCalendars: async (parent, args, context, info) => {
+      // TODO: handle bad response?
+      const res = await calendar.calendarList.list();
+
+      return res.data.items;
+    },
     me: async (parent, args, context, info) => {
       // TODO: handle bad response?
       return context.models.user.findByPk(1);
