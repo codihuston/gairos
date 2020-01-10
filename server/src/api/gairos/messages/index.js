45969b9f2b0ea24cbea5aaa53b529f51df456f59
Huston <cdr95985@ad.wku.edu>
2020-01-10 12:03:31 -0600

    refactor(server): remove debug info from model modules

--- server/src/api/gairos/messages/index.js
+++ server/src/api/gairos/messages/index.js
@@ -1,11 +1,5 @@
 import model from "./model";
 
-console.log("BEING IMPORTED A");
-
-export const lol = "LMFAO";
 export default {
-  model,
-  // resolvers: "QWEQWE",
-  // typeDefs: "asdasdSA",
-  name: "MESSAGES"
+  model
 };
--- server/src/api/gairos/users/index.js
+++ server/src/api/gairos/users/index.js
@@ -2,12 +2,8 @@ import { loadGQLFile } from "../../../utils";
 import model from "./model";
 import resolvers from "./resolvers";
 
-const t = loadGQLFile(__dirname, "schema.graphql");
-console.log("BEING IMPORTED B");
-console.log("YYY LOGGER", t);
 export default {
   model,
-  typeDefs: t,
-  resolvers,
-  name: "USERS"
+  typeDefs: loadGQLFile(__dirname, "schema.graphql"),
+  resolvers
 };
