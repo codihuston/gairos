d24d6c2f26e833a3d4b1fc09bb676f07ff0414f9
Codi Huston (Home) <codi.huston540@gmail.com>
2020-01-21 13:52:31 -0600

    test(server): fix test for reduceTaskHistory for users

--- server/src/api/gairos/users/test/index.js
+++ server/src/api/gairos/users/test/index.js
@@ -28,28 +28,39 @@ export const mockResponses = {
   reduceTaskHistory: {
     input: [
       {
-        id: "1bdc487a-8ad7-4264-b28d-d02dbbef786a",
-        name: "Sample Task",
-        createdAt: "2020-01-18T20:32:37.189Z",
-        updatedAt: "2020-01-18T20:32:37.189Z",
-        userTaskHistory: {
-          userId: "0bdc487a-8ad7-4264-b28d-d02dbbef787b",
-          taskId: "1bdc487a-8ad7-4264-b28d-d02dbbef786a",
-          startTime: "2020-01-18T20:34:11.600Z",
-          endTime: "2020-01-18T20:34:11.600Z",
-          createdAt: "2020-01-18T20:34:11.605Z",
-          updatedAt: "2020-01-18T20:34:11.605Z"
+        id: 1,
+        userId: "0bdc487a-8ad7-4264-b28d-d02dbbef787b",
+        taskId: "1bdc487a-8ad7-4264-b28d-d02dbbef786a",
+        startTime: "2020-01-20T21:52:44.815Z",
+        endTime: "2020-01-20T21:52:44.815Z",
+        createdAt: "2020-01-20T21:52:44.815Z",
+        updatedAt: "2020-01-20T21:52:44.815Z",
+        task: {
+          id: "1bdc487a-8ad7-4264-b28d-d02dbbef786a",
+          name: "Sample Task",
+          createdAt: "2020-01-20T21:52:44.760Z",
+          updatedAt: "2020-01-20T21:52:44.760Z",
+          taskUser: {
+            userId: "0bdc487a-8ad7-4264-b28d-d02dbbef787b",
+            taskId: "1bdc487a-8ad7-4264-b28d-d02dbbef786a",
+            description: "some description",
+            isPublic: true,
+            createdAt: "2020-01-20T21:52:44.808Z",
+            updatedAt: "2020-01-20T21:52:44.808Z"
+          }
         }
       }
     ],
     output: [
       {
-        id: "1bdc487a-8ad7-4264-b28d-d02dbbef786a",
+        userId: "0bdc487a-8ad7-4264-b28d-d02dbbef787b",
+        taskId: "1bdc487a-8ad7-4264-b28d-d02dbbef786a",
         name: "Sample Task",
-        startTime: "2020-01-18T20:34:11.600Z",
-        endTime: "2020-01-18T20:34:11.600Z",
-        createdAt: "2020-01-18T20:34:11.605Z",
-        updatedAt: "2020-01-18T20:34:11.605Z"
+        description: "some description",
+        startTime: "2020-01-20T21:52:44.815Z",
+        endTime: "2020-01-20T21:52:44.815Z",
+        createdAt: "2020-01-20T21:52:44.815Z",
+        updatedAt: "2020-01-20T21:52:44.815Z"
       }
     ]
   }
