4be990f1aaf4fbacad586d62a6afd023863f2969
Huston <codi.huston@wku.edu>
2020-01-29 14:09:29 -0600

    test(server): add to global test setup file to fix harmless error
    - when loading sequelize in jest tests, an error is thrown "Error:
    cannot find module 'pg-native'"
    - this appears to be harmless for now at least
    - this is described in depth more here:
    https://github.com/sequelize/sequelize/issues/3781

--- server/src/test/index.js
+++ server/src/test/index.js
@@ -3,3 +3,7 @@
  * needed. This file is executed prior to all files.
  * ASYNCHRONOUS operations are not supported.
  */
+// work around for removing harmless error when loading sequelize during tests
+// see: https://github.com/sequelize/sequelize/issues/3781#issuecomment-522198459
+import pg from "pg";
+delete pg.native;
