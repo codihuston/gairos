ab108d0b5148b0cf2f5e7174bf8541e6b64a320a
Huston <codi.huston@wku.edu>
2020-02-04 11:08:20 -0600

    refactor(server): server /auth/google now redirects to client login uri
    - the client login uri is provided by environment variables
    - there, the client should send a request to the graphql endpoint; if it
    exceeds, the user is successfully logged in!
    
    refs #28
    closes #1

--- server/src/routes/auth.js
+++ server/src/routes/auth.js
@@ -1,15 +1,18 @@
 import express from "express";
+import debugLib from "debug";
+
 import { oauth2Client, url } from "../services/auth/google";
 import { GooglePeople, GoogleCalendar } from "../api";
 import { models } from "../api";
 
+const debug = debugLib("server:routes/auth");
 const router = express.Router();
 
 /**
  *  GET /auth/google
  */
 router.get("/google", function(req, res) {
-  console.log(oauth2Client, url);
+  debug(oauth2Client, url);
   res.redirect(url);
 });
 
@@ -25,7 +28,7 @@ router.get("/google/cb", async function(req, response, next) {
 
     oauth2Client.setCredentials(tokens);
 
-    console.log("session", req.session);
+    debug("session", req.session);
 
     const people = await GooglePeople.people.get({
       resourceName: "people/me",
@@ -36,17 +39,23 @@ router.get("/google/cb", async function(req, response, next) {
     const user = await models.user.createFromGoogleProfile(people.data);
 
     // if no errors, init a session
-    console.log("store user in session", user);
+    debug("store user in session", user);
     req.session.isAuthenticated = true;
     req.session.tokens = tokens;
     req.session.user = user;
 
     // test getting calendars
-    response.json({
-      // TODO: store user info in session?
+    debug({
       people,
       calendars: await GoogleCalendar.calendarList.list()
     });
+
+    // now redirect to the front-end login uri, where it will
+    // send a query to the graphql endpoint; if it succeeds, so too, should
+    // the login on the client side
+    response.redirect(
+      `${process.env.APP_CLIENT_HTTP_SCHEME}://${process.env.APP_CLIENT_HOST}:${process.env.APP_CLIENT_PORT}/${process.env.APP_CLIENT_LOGIN_URI}`
+    );
   } catch (e) {
     next(e);
   }
