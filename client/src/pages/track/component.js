542b5931e36bdad9aa9be217e4bd74de1ffbf735
Huston <codi.huston@wku.edu>
2020-02-17 13:06:43 -0600

    feat(client): can track any given task once

--- client/src/pages/track/component.js
+++ client/src/pages/track/component.js
@@ -1,16 +1,39 @@
-import React from "react";
+import React, { useState } from "react";
 
 import GetTasks from "../../graphql/queries/hooks/get-tasks";
 import { component as TaskSelect } from "../../components/select-text";
 import { component as Loading } from "../../components/loading";
+import { component as Tracker } from "../../components/tracker";
 
 export default function Home() {
   const { error, data, loading } = GetTasks({
     fetchPolicy: "cache-and-network"
   });
+  const [trackers, setTrackers] = useState([]);
+  const [message, setMessage] = useState(null);
 
   const handleSelect = (e, data) => {
-    console.log("Selected ", data);
+    setMessage(null);
+    let shouldTrack = true;
+    // if task is not already tracked
+    for (let i = 0; i < trackers.length; i++) {
+      const taskIsAlreadyTracked =
+        data.id && trackers[i] && trackers[i].id && trackers[i].id === data.id;
+      if (taskIsAlreadyTracked) {
+        shouldTrack = false;
+        break;
+      }
+    }
+    // track it
+    if (shouldTrack) {
+      setTrackers([...trackers, data]);
+    } else {
+      const msg =
+        `Task '${data.name}' is already being tracked. You may not track the` +
+        ` same task more than once.`;
+      console.warn(msg);
+      setMessage(msg);
+    }
   };
 
   if (loading) {
@@ -28,10 +51,10 @@ export default function Home() {
         displayMember="name"
         handleSelect={handleSelect}
       />
-      <div>Players go here</div>
-      <div>Players go here</div>
-      <div>Players go here</div>
-      <div>Players go here</div>
+      {message ? <div>{message}</div> : null}
+      {trackers.map(task => (
+        <Tracker key={task.id} task={task}></Tracker>
+      ))}
     </div>
   );
 }
