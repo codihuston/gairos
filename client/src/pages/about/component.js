a4c31fa3216a9e285a95b13d64ae3e321f4e80d5
Huston <codi.huston@wku.edu>
2020-02-20 16:35:39 -0600

    style(client): improve verbiage for /home and /about

--- client/src/pages/about/component.js
+++ client/src/pages/about/component.js
@@ -1,12 +1,33 @@
 import React from "react";
 
+import { APP_NAME } from "../../config";
+
 export default function Home() {
   return (
     <div>
+      <h2>About {APP_NAME}</h2>
+      <h3>What does it do?</h3>
       <p>
-        This is a project that is a work in progress, developed by Codi Huston.
-        The name "Gairos" is the name of the project itself; the finalized name
-        of this app has not yet been determined.
+        {APP_NAME} is designed to enable you to track your daily tasks in a
+        simple manner. On top of that, your tasks will be automagically synced
+        to your Google Calendar! We will also provide you with reports so that
+        you can see what you are spending your time on, so that you can manage
+        your time better.
+      </p>
+      <h3>Why use it?</h3>
+      <p>
+        If statistics is your thing, if you just want to get better at managing
+        your time, or if you just want to be able to "see" what you have up to
+        from a birds-eye view, then this app is for you!
+      </p>
+      <h2>About the Developer</h2>
+      <p>
+        This is a project was developed by Codi Huston. The name "{APP_NAME}" is
+        the name of the project itself; the finalized name of this app has not
+        yet been determined. You can follow the progress of this app{" "}
+        <a href="http://github.com/codihuston/gairos/">in the repo</a>! Feel
+        free to drop any suggestions there as well =). You are also welcome to{" "}
+        <a href="https://twitter.com/codingwithcodi">tweet at me!</a>
       </p>
     </div>
   );
--- client/src/pages/home/component.js
+++ client/src/pages/home/component.js
@@ -1,5 +1,21 @@
 import React from "react";
+import { Jumbotron } from "react-bootstrap";
+import { Link } from "react-router-dom";
 
 export default function Home() {
-  return <div>TODO: show reports for this week, link to history!</div>;
+  return (
+    <div>
+      <Jumbotron>
+        <h1>Welcome to Gairos!</h1>
+        <p>
+          Begin <Link to="/track">tracking your tasks</Link> now!
+        </p>
+      </Jumbotron>
+      <p>
+        In the near future, weekly reports on your task tracking progess will be
+        displayed here =). For now, you can view them in{" "}
+        <Link to="/reports">your reports secion!</Link>
+      </p>
+    </div>
+  );
 }
