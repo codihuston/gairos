8c67de273b754254d17433c43cf7129f89ea8be1
Huston <codi.huston@wku.edu>
2020-02-20 16:42:51 -0600

    style(client): basic style for login component

--- client/src/pages/login/component.js
+++ client/src/pages/login/component.js
@@ -1,7 +1,26 @@
 import React from "react";
 import GoogleSignInButton from "../../components/google-login-button";
 
+import { APP_NAME } from "../../config";
+
 export default function Login() {
   // TODO: style this landing page!
-  return <GoogleSignInButton />;
+  return (
+    <div>
+      <div
+        className="m-auto text-center"
+        style={{
+          width: "50vh"
+        }}
+      >
+        <h1 className="text-center">Welcome to {APP_NAME}!</h1>
+        <p className="text-center">
+          Please sign in with your google account below!
+        </p>
+        <div className="m-auto">
+          <GoogleSignInButton />
+        </div>
+      </div>
+    </div>
+  );
 }
