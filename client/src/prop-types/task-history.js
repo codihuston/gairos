aef7f4758aa0a31b67f912344f49096200d3eaa8
Huston <codi.huston@wku.edu>
2020-03-02 13:47:30 -0600

    refactor(client): update PropTypes on task-history

--- client/src/prop-types/task-history.js
+++ client/src/prop-types/task-history.js
@@ -1,7 +1,10 @@
 import PropTypes from "prop-types";
 
+import UserTaskInfoPropTypes from "./user-task-info";
+
 export default PropTypes.shape({
   id: PropTypes.string,
   startTime: PropTypes.string,
-  endTime: PropTypes.string
+  endTime: PropTypes.string,
+  userTaskInfo: UserTaskInfoPropTypes
 });
